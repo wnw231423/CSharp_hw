169d91fcd131ae77b4124d00d5a44f76df6f8824
wnw231423 <2684722187@qq.com>
2025-03-13 21:03:01 +0800

    ex4 initial

---
+++ assignment4/assignment4/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment4
+{
+    public class LinkedList<T>
+    {
+        private class Node<T>
+        { 
+            public T Value { get; set; }
+            public Node<T>? Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private Node<T> head;
+        private Node<T> tail;
+
+        public LinkedList() 
+        {
+            
+        }
+
+    }
+}
